77c141ca6701c13f760dab503fc26fa42f23b400
Daniel Fernandez <danfernandez@intermarkit.es>
2024-04-08 14:57:32 +0200

    Fin sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/Components/counter.module';
 
 import { AppComponent } from './app.component';
+import { HeroesModules } from './heroes/heroes.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModules
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/counter/Components/counter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./Counter/counter.component";
+
+@NgModule({
+  declarations:[
+    CounterComponent
+  ],
+  exports:[
+    CounterComponent
+  ]
+})
+
+export class CounterModule{}
